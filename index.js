6996d14ae899220534d47892a2fca43a171a87a5
Asadbek Rajabov <asadbekrajaboff1337@gmail.com>
2025-05-21 13:41:25 +0500

    feat: line size adjustment added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const color = document.getElementById('color');
+const size = document.getElementById('size');
 
 canvas.width = 500;
 canvas.height = 500;
@@ -11,22 +12,25 @@ ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 let isMouseMoving = false;
 
-canvas.addEventListener('click', (e) => {
-  isMouseMoving = !isMouseMoving;
-  if (isMouseMoving) {
-    const rect = canvas.getBoundingClientRect();
-    ctx.beginPath();
-    ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
-  }else {
-    ctx.closePath();
-  }
-
+canvas.addEventListener('dblclick', (e) => {
+  isMouseMoving = true;
+  const rect = canvas.getBoundingClientRect();
+  ctx.beginPath();
+  ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
 })
+
+
 
 canvas.addEventListener('mousemove', (e) => {
   if(!isMouseMoving) return;
   const rect = canvas.getBoundingClientRect();
+  ctx.lineWidth = size.value
   ctx.lineTo(e.clientX - rect.left, e.clientY - rect.top);
   ctx.strokeStyle = color.value
   ctx.stroke();
+})
+
+canvas.addEventListener('click', (e) => {
+  ctx.closePath();
+  isMouseMoving = false;
 })
